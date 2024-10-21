861a9f46b687d077f733d6e31d5f0eee7113ce57
Saanvi <saanviprashantkumar@gmail.com>
2024-10-21 14:04:36 +0530

    Initial Commit

---
+++ Eclipse/EclipseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EclipseApp.swift
+//  Eclipse
+//
+//  Created by user@87 on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EclipseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
